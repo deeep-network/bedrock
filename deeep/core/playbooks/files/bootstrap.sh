a272c29bd59675b4da77ac1dd631f6597b24dd08
Anthony Anderson <fnanderson93@gmail.com>
2025-01-14 23:21:39 -0500

    fix: :bug: wait for ansible-rulebook with bootstrap.sh

--- deeep/core/playbooks/files/bootstrap.sh
+++ deeep/core/playbooks/files/bootstrap.sh
@@ -6,9 +6,16 @@ if ! hab svc status | grep -q "ilert-heartbeat"; then
 fi
 
 if ! hab svc status | grep -q "ansible"; then
-  echo "Installing step package..."
+  echo "Installing step-cli package..."
   hab pkg install deeep-network/step
 
   echo "Loading ansible service..."
   hab svc load deeep-network/ansible
+
+  # it takes a while for ansible-rulebook to spin up so lets
+  # wait for it before calling the endpoint
+  until curl -s http://127.0.0.1:33337 >/dev/null 2>&1; do
+    echo "Waiting on ansible-rulebook..."
+    sleep 2
+  done
 fi
